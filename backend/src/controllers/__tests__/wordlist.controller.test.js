3972bb5ae6f1b3335af29d5072c4ca6a4c80a64f
Lucas Simão <lsimaocosta@gmail.com>
2019-08-23 20:11:27 -0300

    sending authorization header with all requests

--- backend/src/controllers/__tests__/wordlist.controller.test.js
+++ backend/src/controllers/__tests__/wordlist.controller.test.js
@@ -40,6 +40,7 @@ describe("Wordlist's restful API test", () => {
     test('POST to / should return http status 201 if it was able to create a new Wordlist', () => {
         return request(app)
             .post('/wordlists')
+            .set('Authorization', `Bearer ${jwtToken}` )
             .send(wordlist)
             .expect('Content-Type', /json/)
             .expect(201)
@@ -56,7 +57,11 @@ describe("Wordlist's restful API test", () => {
 
     test('PATCH to /:id should return the status 204 if it was able to partially update a wordlist', async () => {
        
-        const response = await request(app).post('/wordlists').send(wordlist)
+        const response = await request(app)
+                            .post('/wordlists')
+                            .set('Authorization', `Bearer ${jwtToken}` )
+                            .send(wordlist)
+                            
         expect(response.status).toBe(201)
 
         const link = response.header['Link']
@@ -66,6 +71,7 @@ describe("Wordlist's restful API test", () => {
 
         return request(app)
             .patch(`/wordlist/${id}`)
+            .set('Authorization', `Bearer ${jwtToken}` )
             .send({
                 words: [ 'win', 'growth']
             })
@@ -79,7 +85,11 @@ describe("Wordlist's restful API test", () => {
 
     test('POST to /:id/words should return the status 204 if it was able to add a new word to a wordlist', async () => {
        
-        const response = await request(app).post('/wordlists').send({ ...wordlist, words: ['word1','word2']})
+        const response = await request(app)
+                        .post('/wordlists')
+                        .set('Authorization', `Bearer ${jwtToken}` )
+                        .send({ ...wordlist, words: ['word1','word2']})
+
         expect(response.status).toBe(201)
 
         const link = response.header['Link']
@@ -89,6 +99,7 @@ describe("Wordlist's restful API test", () => {
 
         return request(app)
             .post(`/wordlist/${id}/words`)
+            .set('Authorization', `Bearer ${jwtToken}` )
             .send({
                 words: [ 'win', 'growth']
             })
@@ -102,7 +113,10 @@ describe("Wordlist's restful API test", () => {
 
 
     test('DELETE to /wordlist/:id should return the status 204 if it was able to delete a wordlist', async () => {
-        const response = await request(app).post('/wordlist').send(wordlist)
+        const response = await request(app).post('/wordlist')
+                .set('Authorization', `Bearer ${jwtToken}` )
+                .send(wordlist)
+
         expect(response.status).toBe(201)
 
         const link = response.header['Link']
@@ -113,14 +127,21 @@ describe("Wordlist's restful API test", () => {
         expect(freelaService.get(id)).not.toBeNull()
 
         // submting the delete
-        const response2 = await request(app).delete(`/wordlist/${id}`)
+        const response2 = await request(app)
+            .set('Authorization', `Bearer ${jwtToken}` )
+            .delete(`/wordlist/${id}`);
+
         expect(response2.status).toBe(204)
         expect(freelaService.get(id)).toBeNull()
     })
 
     test('DELETE to /:id/words/:id should return the status 204 if it was able to remove a word from the wordlist', async () => {
        
-        const response = await request(app).post('/wordlists').send({ ...wordlist, words: ['word1','word2']})
+        const response = await request(app)
+                            .post('/wordlists')
+                            .set('Authorization', `Bearer ${jwtToken}` )
+                            .send({ ...wordlist, words: ['word1','word2']})
+    
         expect(response.status).toBe(201)
 
         const link = response.header['Link']
@@ -130,6 +151,7 @@ describe("Wordlist's restful API test", () => {
 
         return request(app)
             .delete(`/wordlist/${id}/words/0`)
+            .set('Authorization', `Bearer ${jwtToken}` )
             .expect('Content-Type', /json/)
             .expect(204) 
             .expect(async res => {
