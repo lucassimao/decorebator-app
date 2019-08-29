f7949a870967ef36b4b205119bc7c0639a0b2093
Lucas Simão <lsimaocosta@gmail.com>
2019-08-29 06:50:24 -0300

    WIP auth.service

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,55 @@
+
+const bcrypt = require('bcrypt');
+const jwtBuilder = require('jwt-builder');
+const UserDao = require('../dao/user.dao');
+
+const register = (email, password) => {
+
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, 10, function (err, hash) {
+            if (err)
+                reject(err)
+            else
+                resolve(hash)
+        });
+    })
+        .then(encrypted_password => UserDao.create({ email, encrypted_password }))
+
+}
+
+const doLogin = (email, password) => {
+
+    return UserDao.findOne({ email }, 'encrypted_password')
+        .exec()
+        .then(user => {
+            const doesMatch = bcrypt.compareSync(password, user.encrypted_password)
+            if (doesMatch) {
+                return user;
+            } else {
+                throw 'wrong password'
+            }
+        })
+        .then(user => {
+
+            return jwtBuilder({
+                algorithm: 'HS256',
+                secret: 'super-secret',
+                iat: true,
+                nbf: true,
+                exp: 3600,
+                iss: 'https://auth.vandium.io',
+                userId: user._id,
+                headers: {
+                    header1: 'ok'
+                },
+                claims: {
+                    claim1: 'ok'
+                }
+            });
+
+        })
+}
+
+const removeAccount = (email) => UserDao.deleteOne({ email }).exec()
+
+module.exports = { register, doLogin, removeAccount }
