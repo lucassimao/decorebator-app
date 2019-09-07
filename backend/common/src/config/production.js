2e5cc475de0f202311c82400eb6e4eaa23e302a7
Lucas Simão <lsimaocosta@gmail.com>
2019-09-07 14:52:30 -0300

    adding production configuration

--- backend/common/src/config/production.js
+++ backend/common/src/config/production.js
@@ -1,1 +1,11 @@
-throw new Error('not implemented yet')
+if (!process.env.MONGO_DB_URL)
+    throw 'Mongo db url was not provided!';
+
+if (!process.env.HTTP_PORT)
+    throw 'Http server port must be provided!';
+
+module.exports = {
+    dbUrl: process.env.MONGO_DB_URL,
+    port: process.env.HTTP_PORT,
+    httpDomain: `https://decorebator.com`
+}
