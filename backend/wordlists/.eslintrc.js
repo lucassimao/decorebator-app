7a02fb142901e9f4f962ca660a5bc78a267e67a6
Lucas Simão <lsimaocosta@gmail.com>
2019-09-06 22:45:15 -0300

    jest eslint rules configuration

--- backend/wordlists/.eslintrc.js
+++ backend/wordlists/.eslintrc.js
@@ -15,5 +15,10 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
-    }
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
 };
