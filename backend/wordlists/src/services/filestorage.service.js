29abcd3f0793b105226d814bff4002e3be76bdcb
Lucas Simão <lsimaocosta@gmail.com>
2019-09-06 21:22:42 -0300

    uploading file to aws s3 in production mode. Saving to local file system temp dir on dev and testing mode

--- backend/wordlists/src/services/filestorage.service.js
+++ backend/wordlists/src/services/filestorage.service.js
@@ -1,24 +1,51 @@
+const crypto = require('crypto');
 const os = require("os");
 const path = require("path");
 const shortid = require("shortid");
 const fsPromises = require('fs').promises;
+const AWS = require('aws-sdk');
+
 const config = require("decorebator-common").config;
 
 
-const store = async (fileName, base64EncodedFile) => {
+/**
+ * persists a file. In production environment, sends the data to amazon S3, otherwise stores in the local system's temp dir 
+ * 
+ * @param {Oject} user The user storing the file
+ * @param {mongoose.Types.ObjectId} user._id The user id 
+ * @param {fileName} fileName The original filename sent by the user
+ * @param {base64EncodedFile} base64EncodedFile The binary data encoded as base64 format
+ * 
+ * @returns {string} link to the file
+ */
+const store = async (user, fileName, base64EncodedFile) => {
+  const ext = path.extname(fileName);
+  const buffer = Buffer.from(base64EncodedFile, 'base64');
+
   if (config.isDev || config.isTest) {
-    const newFileName = shortid.generate();
-    const ext = path.extname(fileName);
-    const localPath = path.join(os.tmpdir(),newFileName + ext)
-    const buffer = Buffer.from(base64EncodedFile, 'base64');
+    const localPath = path.join(os.tmpdir(), shortid.generate() + ext)
+
     await fsPromises.writeFile(localPath, buffer);
     return localPath
   } else {
 
+    const bucketName = String(user._id);
+    const keyName = shortid.generate() + ext;
+    const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+    await s3.createBucket({ Bucket: bucketName }).promise();
+
+    const hash = crypto.createHash('md5');
+    hash.update(buffer);
+    const md5sum = hash.digest('base64');
+
+    var objectParams = { ACL: "public-read", Bucket: bucketName, Key: keyName, Body: buffer, ContentMD5: md5sum };
+    await s3.putObject(objectParams).promise();
+
+    return `https://${bucketName}.s3.amazonaws.com/${keyName}`;
   }
 };
 
-
 module.exports = {
-    store
+  store
 }
