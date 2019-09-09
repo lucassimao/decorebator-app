96eab444b32fd905c7bc6347018a09ce56e90d71
Lucas Simão <lsimaocosta@gmail.com>
2019-09-09 11:22:28 -0300

    filestorage.service tests

---
+++ backend/wordlists/src/services/__tests__/filestorage.service.tests.js
@@ -0,0 +1,68 @@
+const https = require("https");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const crypto = require("crypto");
+const os = require("os");
+const service = require("../filestorage.service");
+
+describe("File storage service tests", () => {
+  it("on non production environment, file should be stored on the local system's temp dir", async done => {
+    const filepath = __dirname + "/fixtures/file.pdf";
+
+    const originalFileMd5 = await getFileMd5(filepath);
+    const content = await readFileContentAsBase64(filepath);
+    const copiedFilepath = await service.store({ _id: 123 }, "file.pdf", content);
+    const copiedFileMd5 = await getFileMd5(copiedFilepath);
+
+    await fsPromises.access(filepath, fs.constants.R_OK);
+    expect(originalFileMd5).toBe(copiedFileMd5);
+    expect(copiedFilepath).toMatch(new RegExp(`^${os.tmpdir()}/`));
+    done();
+  });
+
+  it("should upload the file in production to amazon s3", async done => {
+    const filepath = __dirname + "/fixtures/file.pdf";
+    const originalFileMd5 = await getFileMd5(filepath);
+    const content = await readFileContentAsBase64(filepath);
+
+    const uri = await service.storeOnAmazonS3("wordlists-test-03410718389", "file.pdf", content);
+    https.get(uri, res => {
+      expect(res.statusCode).toBe(200);
+      const contentLength = parseInt(res.headers["content-length"]);
+
+      const buffers = [];
+      res.on("data", chunk => {
+        buffers.push(chunk);
+      });
+      res.on("end", async () => {
+        const downloadFileBuffer = Buffer.concat(buffers, contentLength);
+        const hash = crypto.createHash("md5");
+        hash.update(downloadFileBuffer);
+
+        expect(originalFileMd5).toBe(hash.digest("base64"));
+        await service.removeS3File(uri);
+        done();
+      });
+    });
+  });
+});
+
+async function readFileContentAsBase64(filepath) {
+  let fileHandle;
+
+  try {
+    fileHandle = await fsPromises.open(filepath, "r");
+    const buffer = await fileHandle.readFile();
+    const base64Content = buffer.toString("base64");
+    return base64Content;
+  } finally {
+    if (fileHandle) fileHandle.close();
+  }
+}
+
+async function getFileMd5(filepath) {
+  const base64Content = await readFileContentAsBase64(filepath);
+  const hash = crypto.createHash("md5");
+  hash.update(base64Content);
+  return hash.digest("base64");
+}
--- backend/wordlists/src/services/filestorage.service.js
+++ backend/wordlists/src/services/filestorage.service.js
@@ -1,50 +1,99 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 const os = require("os");
 const path = require("path");
 const shortid = require("shortid");
-const fsPromises = require('fs').promises;
-const AWS = require('aws-sdk');
+const fsPromises = require("fs").promises;
+const AWS = require("aws-sdk");
 const config = require("decorebator-common").config;
+
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 
 /**
- * persists a file. In production environment, sends the data to amazon S3, otherwise stores in the local system's temp dir 
- * 
+ * persists a file. In production environment, sends the data to amazon S3, otherwise stores in the local system's temp dir
+ *
  * @param {object} user The user storing the file
- * @param {mongoose.Types.ObjectId} user._id The user id 
+ * @param {mongoose.Types.ObjectId} user._id The user id
  * @param {string} fileName The original filename sent by the user
  * @param {string} base64EncodedFile The binary data encoded as base64 format
- * 
+ *
  * @returns {string} link to the file
  */
 const store = async (user, fileName, base64EncodedFile) => {
   const ext = path.extname(fileName);
-  const buffer = Buffer.from(base64EncodedFile, 'base64');
+  const buffer = Buffer.from(base64EncodedFile, "base64");
 
   if (config.isDev || config.isTest) {
-    const localPath = path.join(os.tmpdir(), shortid.generate() + ext)
+    const localPath = path.join(os.tmpdir(), shortid.generate() + ext);
 
     await fsPromises.writeFile(localPath, buffer);
-    return localPath
+    return localPath;
   } else {
-
-    const bucketName = String(user._id);
-    const keyName = shortid.generate() + ext;
-    const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-
-    await s3.createBucket({ Bucket: bucketName }).promise();
-
-    const hash = crypto.createHash('md5');
-    hash.update(buffer);
-    const md5sum = hash.digest('base64');
-
-    var objectParams = { ACL: "public-read", Bucket: bucketName, Key: keyName, Body: buffer, ContentMD5: md5sum };
-    await s3.putObject(objectParams).promise();
-
-    return `https://${bucketName}.s3.amazonaws.com/${keyName}`;
+    return await storeOnAmazonS3(user._id, ext, buffer);
   }
 };
 
-module.exports = {
-  store
+/**
+ *
+ * store a buffer as a new file on amazon s3
+ *
+ * @param {mongoose.Types.ObjectId} userId The user id
+ * @param {string} extension file extension e.g: '.pdf'
+ * @param {Buffer} buffer file content
+ */
+async function storeOnAmazonS3(userId, extension, buffer) {
+  const bucketName = String(userId);
+  const keyName = shortid.generate() + extension;
+
+  await s3.createBucket({ Bucket: bucketName }).promise();
+
+  const hash = crypto.createHash("md5");
+  hash.update(buffer);
+  const md5sum = hash.digest("base64");
+
+  var objectParams = {
+    ACL: "public-read",
+    Bucket: bucketName,
+    Key: keyName,
+    Body: buffer,
+    ContentMD5: md5sum
+  };
+  await s3.putObject(objectParams).promise();
+
+  return `https://${bucketName}.s3.amazonaws.com/${keyName}`;
 }
+
+async function removeS3File(uri) {
+  const regex = /https:\/\/(\S+).s3.amazonaws.com\/(\S+)$/;
+  const match = regex.exec(uri);
+  const Bucket = match[1],
+    Key = match[2];
+
+  const params = {
+    Bucket,
+    Key
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.deleteObject(params, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+let api;
+if (config.isTest) {
+  api = {
+    store,
+    storeOnAmazonS3,
+    removeS3File
+  };
+} else {
+  api = { store };
+}
+
+module.exports = api;
