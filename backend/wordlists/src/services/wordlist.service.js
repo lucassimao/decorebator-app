7f2d75f5cb47cc4d0a1787b626a672f54c3dbf0e
Lucas Simão <lsimaocosta@gmail.com>
2019-09-10 12:00:26 -0300

    wordlist service api docs

--- backend/wordlists/src/services/wordlist.service.js
+++ backend/wordlists/src/services/wordlist.service.js
@@ -1,27 +1,78 @@
 const config = require("decorebator-common").config;
 const WordlistDao = require("../dao/wordlist.dao");
 
+/**
+ * Returns user's wordlists
+ *
+ * @param {number} pageSize The amount of wordlists to be returned
+ * @param {number} page As the data set is seen as a set of pages, this param represents the requested page
+ * @param {object} user The authenticated user
+ * @param {mongoose.Types.ObjectId} user._id The user id
+ *
+ * @returns {Promise} A promise, which resolves to an array of wordlists
+ */
 const list = ({ pageSize = config.defaultPageSize, page = 0 }, user) => {
   const skip = page > 0 ? (page - 1) * pageSize : 0;
   return WordlistDao.find({ owner: user._id }, null, { limit: pageSize, skip });
 };
 
+/**
+ * Register a new wordlist
+ *
+ * @param {object} wordlist the wordlist to be persisted
+ * @param {object} user The authenticated user, owner of the new wordlist
+ * @param {mongoose.Types.ObjectId} user._id The user id
+ *
+ * @returns {Promise} A promise, which resolves to the persisted object
+ */
 const save = (wordlist, user) => {
   return WordlistDao.create({ ...wordlist, owner: user._id });
 };
 
+/**
+ * Search the database for a wordlist with an _id
+ *
+ * @param {string|mongoose.Types.ObjectId} id the wordlist id
+ * @param {object} user The authenticated user, owner of the wordlist
+ * @param {mongoose.Types.ObjectId} user._id The user id
+ *
+ * @returns {Promise} A promise, which resolves to the persisted object, if found
+ */
 const get = (id, user) => {
   return WordlistDao.findOne({ _id: id, owner: user._id });
 };
 
+/**
+ * Updates a existing wordlist
+ *
+ * @param {string|mongoose.Types.ObjectId} id The id of the object to be updated
+ * @param {object} user The authenticated user, owner of the wordlist
+ * @param {mongoose.Types.ObjectId} user._id The user id
+ *
+ * @returns {Promise} A promise, which resolves to an operation info object from MongoDB
+ */
 const update = (id, updateObj, user) => {
   return WordlistDao.updateOne({ _id: id, owner: user._id }, updateObj);
 };
 
+/**
+ * Remove all existing wordlists. Only available on testing environment
+ *
+ * @returns {Promise} A promise, which resolves to an operation info object from MongoDB
+ */
 const deleteAll = () => {
   return WordlistDao.deleteMany({});
 };
 
+/**
+ * removes an existing wordlist
+ *
+ * @param {string|mongoose.Types.ObjectId} id The id of the object to be updated
+ * @param {object} user The authenticated user, owner of the wordlist
+ * @param {mongoose.Types.ObjectId} user._id The user id
+ *
+ * @returns {Promise} A promise, which resolves to an operation info object from MongoDB
+ */
 const remove = (id, user) => {
   return WordlistDao.deleteOne({ _id: id, owner: user._id });
 };
