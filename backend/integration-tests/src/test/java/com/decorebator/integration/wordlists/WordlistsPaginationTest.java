3afa02cf40ccde1539f002f952bfbb922aa7ac3c
Lucas Simão <lsimaocosta@gmail.com>
2019-10-04 13:14:43 -0300

    updating WordlistsPaginationTest impĺementation

--- backend/integration-tests/src/test/java/com/decorebator/integration/wordlists/WordlistsPaginationTest.java
+++ backend/integration-tests/src/test/java/com/decorebator/integration/wordlists/WordlistsPaginationTest.java
@@ -3,18 +3,14 @@ package com.decorebator.integration.wordlists;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
 
+import com.decorebator.integration.EnvironmentRule;
 import com.decorebator.integration.TestUtils;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 
 import io.restassured.RestAssured;
 
@@ -25,38 +21,22 @@ import io.restassured.RestAssured;
 public class WordlistsPaginationTest {
 
     @ClassRule
-    public static DockerComposeContainer environment = new DockerComposeContainer(new File(TestUtils.DOCKER_COMPOSER_YML)).withLocalCompose(true)
-            .withExposedService("wordlists", 3000, Wait.forListeningPort())
-            .withExposedService("auth", 3000, Wait.forListeningPort())
-            .withExposedService("db", 27017, Wait.forListeningPort());
+    public static EnvironmentRule env = new EnvironmentRule(true);
 
     @BeforeClass
     public static void setup() {
-        String wordlistsHost = environment.getServiceHost("wordlists", 3000);
-        int wordlistPort = environment.getServicePort("wordlists", 3000);
-        RestAssured.baseURI = String.format("http://%s:%d/wordlists", wordlistsHost, wordlistPort);
-    }
-
-    @Before
-    public void clearMongoDb() {
-        String mongodbHost = environment.getServiceHost("db", 27017);
-        int mongodbPort = environment.getServicePort("db", 27017);
-        TestUtils.clearMongoDb(mongodbHost,mongodbPort);
+        RestAssured.baseURI = env.getWordlistEndpoint().toString();
     }
 
     @Test
     public void ensureOnly10WordlistsAreReturned() throws MalformedURLException {
-        var authHost = environment.getServiceHost("auth", 3000);
-        var authPort = environment.getServicePort("auth", 3000);
-        var signInEndpoint = new URL("http",authHost,authPort,"/signin");
-        var signUpEndpoint = new URL("http",authHost,authPort,"/signup");
+        var signInEndpoint = env.getSignInEndpoint();
+        var signUpEndpoint = env.getSignUpEndpoint();
 
         var registration = TestUtils.createRandomUser(signUpEndpoint);
         var authorization = TestUtils.signIn(registration.getLogin(), registration.getPassword(), signInEndpoint);
 
-        var wordlistHost = environment.getServiceHost("wordlists", 3000);
-        var wordlistPort = environment.getServicePort("wordlists", 3000);
-        var wordlistsEndpoint = new URL("http",wordlistHost,wordlistPort,"/wordlists");
+        var wordlistsEndpoint = env.getWordlistEndpoint();
 
         // will generate 11 pages, from 0 to 10, and the last page must have a single item
         for(int i=0;i < 101;++i){
