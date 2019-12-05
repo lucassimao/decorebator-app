157b42e563389ce94462aa212b0c67b84728a926
Lucas Simão <lsimaocosta@gmail.com>
2019-12-05 15:33:07 -0300

    formating

--- frontend/src/components/dashboard/Home.js
+++ frontend/src/components/dashboard/Home.js
@@ -4,16 +4,15 @@ import Typography from "@material-ui/core/Typography";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import ListAltRoundedIcon from "@material-ui/icons/ListAltRounded";
 import YouTubeIcon from "@material-ui/icons/YouTube";
-import SpeedDial from '@material-ui/lab/SpeedDial';
-import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDial from "@material-ui/lab/SpeedDial";
+import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
+import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchPublicWordlists, fetchUserWordlists } from "../../thunks/wordlist.thunks";
 import SearchBox from "./SearchBox";
 import Wordlists from "./Wordlists";
-
 
 const useStyles = makeStyles(theme => ({
   sectionHeader: {
@@ -27,30 +26,32 @@ const useStyles = makeStyles(theme => ({
     }
   },
   speedDial: {
-    position: 'fixed',
-    '&.MuiSpeedDial-directionUp': {
+    position: "fixed",
+    "&.MuiSpeedDial-directionUp": {
       bottom: theme.spacing(3),
-      right: theme.spacing(2),
-    },
+      right: theme.spacing(2)
+    }
   },
   tooltip: {
-    width: '140px'
-  },
+    width: "140px"
+  }
 }));
-
-
 
 const NewWordlistLink = React.forwardRef((props, ref) => (
   <Link to="/wordlists/new" innerRef={ref} {...props} />
 ));
 
 const NewWordlistFromYoutubeLink = React.forwardRef((props, ref) => (
-    <Link to="/wordlists/new-from-youtube" innerRef={ref} {...props} />
-  ));
+  <Link to="/wordlists/new-from-youtube" innerRef={ref} {...props} />
+));
 
 const actions = [
-  { icon: <ListAltRoundedIcon />, name: 'New wordlist', component : NewWordlistLink },
-  { icon: <YouTubeIcon />, name: 'From youtube', component : NewWordlistFromYoutubeLink }
+  { icon: <ListAltRoundedIcon color="primary" />, name: "New wordlist", component: NewWordlistLink },
+  {
+    icon: <YouTubeIcon style={{ color: "#f00" }} />,
+    name: "From youtube",
+    component: NewWordlistFromYoutubeLink
+  }
 ];
 
 function Home(props) {
@@ -59,8 +60,7 @@ function Home(props) {
   const [open, setOpen] = React.useState(false);
 
   const handleClose = () => setOpen(false);
-  const handleOpen = () =>  setOpen(true);
-
+  const handleOpen = () => setOpen(true);
 
   useEffect(() => {
     loadUserWordlists();
@@ -84,14 +84,14 @@ function Home(props) {
 
       <Wordlists wordlists={publicWordlists} />
 
-      <SpeedDial 
+      <SpeedDial
         ariaLabel="Main menu"
         className={classes.speedDial}
         icon={<SpeedDialIcon />}
         onClose={handleClose}
         onOpen={handleOpen}
         open={open}
-        direction='up'
+        direction="up"
       >
         {actions.map(action => (
           <SpeedDialAction
@@ -104,7 +104,6 @@ function Home(props) {
           />
         ))}
       </SpeedDial>
-
     </Container>
   );
 }
@@ -119,7 +118,4 @@ const mapDispatchToProps = dispatch => ({
   loadPublicWordlists: () => dispatch(fetchPublicWordlists())
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
