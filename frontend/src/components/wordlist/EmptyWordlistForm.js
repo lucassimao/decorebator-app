9826f23fde7fd22dcfd194bbf3f2093ffe4bc4fa
Lucas Simão <lsimaocosta@gmail.com>
2019-12-25 14:46:55 -0300

    improving EmptyWordlistForm ux

--- frontend/src/components/wordlist/EmptyWordlistForm.js
+++ frontend/src/components/wordlist/EmptyWordlistForm.js
@@ -1,11 +1,11 @@
-import { makeStyles } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Container, Fab, IconButton, makeStyles } from "@material-ui/core";
+import * as colors from "@material-ui/core/colors";
 import Grid from "@material-ui/core/Grid";
 import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
-import Switch from "@material-ui/core/Switch";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import React from "react";
 import useForm from "react-hook-form";
 import { connect } from "react-redux";
@@ -13,31 +13,37 @@ import { Link, useHistory } from "react-router-dom";
 import { HIDE_PROGRESS_MODAL, SHOW_PROGRESS_MODAL } from "../../reducers/progressModal";
 import { SET_ERROR_SNACKBAR, SET_SUCCESS_SNACKBAR } from "../../reducers/snackbar";
 import service from "../../services/wordlist.service";
-import AppBreadcrumb from "../common/AppBreadcrumb";
-import * as colors from "@material-ui/core/colors";
 
-const LANGUAGES = ["English", "Spanish", "Portuguese", "French", "German", "Italian", "Dutch", "Mandarin"];
+
+const LANGUAGES = ["Dutch", "English", "French", "German", "Italian", "Portuguese", "Mandarin", "Spanish",];
 LANGUAGES.sort();
 
 const useStyles = makeStyles(theme => ({
-  form: {
-    padding: theme.spacing(0, 2),
+
+  header: {
+    position: 'relative',
+    marginBottom: theme.spacing(2)
+  },
+  container: {
     height: "100%",
-    display: "flex",
-    flexFlow: "column",
-    justifyContent: "space-between",
-    color: theme.palette.grey[500],
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  form: {
+    borderRadius: theme.shape.borderRadius,
+    flexGrow: 1,
+    backgroundColor: '#fff',
+    padding: theme.spacing(2),
+    boxShadow: "0 7px 14px rgba(0,0,0,0.25)",
     "& label": {
       fontWeight: "bold"
-    }
+    },
+    display: 'flex',
+    flexDirection: 'column',
   },
-  button: {
-    margin: theme.spacing(0, 1)
-  },
-  gridButtons: {
-    marginBottom: theme.spacing(3),
-    backgroundColor: theme.palette.grey[200],
-    padding: theme.spacing(1)
+  btnSave: {
+    marginTop: theme.spacing(5)
   }
 }));
 
@@ -47,18 +53,19 @@ const generateRandomColor = () => {
   return colors[colorNames[randomIdx]][500];
 };
 
-const HomeLink = React.forwardRef((props, ref) => <Link to="/" innerRef={ref} {...props} />);
+const MenuLink = React.forwardRef((props, ref) => <Link to="/wordlists/menu" innerRef={ref} {...props} />);
 
 function EmptyWordlistForm(props) {
   const { onSuccess, onError, showProgressModal, hideProgressModal } = props;
   const classes = useStyles();
   const history = useHistory();
 
-  const { register, handleSubmit, errors } = useForm({
-    defaultValues: { language: "English"}
-  });
+  const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = async data => {
+
+    data.isPrivate = true;
+
     try {
       showProgressModal("Wait ...", "Creating your wordlist");
 
@@ -67,7 +74,7 @@ function EmptyWordlistForm(props) {
       hideProgressModal();
       onSuccess("wordlist created");
       history.push(resourceUri);
-    
+
     } catch (error) {
       hideProgressModal();
       onError(error.message);
@@ -76,63 +83,72 @@ function EmptyWordlistForm(props) {
   };
 
   return (
-    <form className={classes.form} noValidate onSubmit={handleSubmit(onSubmit)}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <AppBreadcrumb />
+    <Container className={classes.container}>
+
+      <header className={classes.header}>
+
+        <IconButton component={MenuLink} size="medium" color="primary"
+          style={{ position: "absolute", top: -5, left: 0, fontWeight: "bold" }}>
+          <ArrowBackIcon />
+        </IconButton>
+
+        <Typography variant="h5" style={{ fontWeight: "bold" }} align="center">
+          New wordlist
+        </Typography>
+      </header>
+
+      <form className={classes.form} noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Grid className={classes.grid} container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              margin="dense"
+              fullWidth
+              autoComplete="off"
+              autoFocus
+              error={Boolean(errors.name)}
+              helperText={errors.name && "Name is required"}
+              name="name"
+              inputRef={register({ required: true })}
+              label="Name"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              multiline
+              rows={5}
+              name="description"
+              inputRef={register}
+              variant="outlined"
+              label="Description"
+            />
+          </Grid>
+          {/* <Grid item xs={12}>
+            <FormControlLabel
+              control={<Switch name="isPrivate" inputRef={register} color="primary" />}
+              label="Private"
+            />
+          </Grid> */}
+          <Grid item xs={12}>
+            <InputLabel htmlFor="language">Language</InputLabel>
+            <Select name="language" placeholder="Language" fullWidth native inputRef={register}>
+              {LANGUAGES.map(lang => (
+                <option key={lang} value={lang}>
+                  {lang}
+                </option>
+              ))}
+              <option value="Other">Other</option>
+            </Select>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            margin="dense"
-            fullWidth
-            autoComplete="off"
-            autoFocus
-            error={Boolean(errors.name)}
-            helperText={errors.name && "Name is required"}
-            name="name"
-            inputRef={register({ required: true })}
-            label="Name"
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            multiline
-            rows={3}
-            name="description"
-            inputRef={register}
-            variant="outlined"
-            label="Description"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Switch name="isPrivate" inputRef={register} color="primary" />}
-            label="Private"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <InputLabel htmlFor="language">Language</InputLabel>
-          <Select name="language" placeholder="Language" fullWidth native inputRef={register}>
-            {LANGUAGES.map(lang => (
-              <option key={lang} value={lang}>
-                {lang}
-              </option>
-            ))}
-            <option value="Other">Other</option>
-          </Select>
-        </Grid>
-      </Grid>
-      <Grid className={classes.gridButtons} container justify="flex-end">
-        <Button component={HomeLink} variant="contained" className={classes.button}>
-          Cancel
-        </Button>
-        <Button type="submit" variant="contained" color="primary" className={classes.button}>
+        <Fab size="large" className={classes.btnSave} type="submit" variant="extended" color="primary">
           Save
-        </Button>
-      </Grid>
-    </form>
+            </Fab>
+
+      </form>
+    </Container>
+
   );
 }
 
