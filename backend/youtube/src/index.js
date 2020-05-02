727c6103461ef6272996700525ad6b162dc302ca
Lucas Simão <lsimaocosta@gmail.com>
2020-05-02 19:45:42 -0300

    securing graphql requests

--- backend/youtube/src/index.js
+++ backend/youtube/src/index.js
@@ -1,15 +1,26 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schemas');
 const resolvers = require('./resolvers');
-const { logger, port } = require('./config');
+const { logger, port, isDev } = require('./config');
 const db = require('./db');
+const AuthService = require('./services/auth.service');
 
 db.connect()
     .then(() => {
-        const server = new ApolloServer({ typeDefs, resolvers, logger });
+        const server = new ApolloServer({
+            typeDefs, resolvers, logger, context: contextFunction
+        });
         server.listen({ port }).then(({ url }) => {
             logger.info(`🚀 Server ready at ${url}`);
         });
     })
     .catch(logger.error);
 
+const contextFunction = ({ req }) => {
+    const { operationName } = req.body || {};
+    if (operationName == 'IntrospectionQuery' && isDev) {
+        return
+    }
+
+    return AuthService.authenticate(req.headers.authorization);
+}
---
+++ backend/youtube/src/services/auth.service.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const { AuthenticationError, ApolloError } = require('apollo-server');
+const { logger, jwtSecretKey } = require('../config');
+const UserDao = require('../dao/user.dao');
+
+
+/**
+ * Decodes the jwt token and returns a object with the name, email and _id of the user, if it exists
+ * 
+ * @param {String} jwtToken A base64 encoded jwt token
+ * @returns A promise which resolves to the authenticating user
+ * @see https://github.com/auth0/node-jsonwebtoken/
+ */
+function authenticate(jwtToken) {
+
+    if (typeof jwtToken != 'string' || !jwtToken.trim()) {
+        return;
+    }
+
+    if (jwtToken.startsWith('Bearer')){
+        jwtToken = jwtToken.slice(7);
+    }
+
+    return new Promise((resolve, reject) => {
+
+        jwt.verify(jwtToken, jwtSecretKey, async (err, decoded) => {
+            const { userId } = decoded || {};
+
+            if (err) {
+                logger.error(err);
+                reject(new ApolloError(err.message, err.name));
+            } else {
+                const user = await UserDao.findById(userId, "name country email");
+
+                if (user) {
+                    resolve(user);
+                } else {
+                    reject(new AuthenticationError('Invalid credentials'))
+                }
+            }
+
+        });
+    })
+}
+
+module.exports = { authenticate }
