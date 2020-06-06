ac75b7b4cb6b7845e5e7ae64b1156330995d5638
Lucas Simão <lsimaocosta@gmail.com>
2020-06-06 17:09:31 -0300

    feat: adding a index.ts inside lib/dao to export database and domain classes

---
+++ backend/common/lib/dao/index.ts
@@ -0,0 +1,11 @@
+import Database from "./db";
+import { Image } from "./image";
+import { User } from "./user";
+import { Word } from "./word";
+import { Wordlist } from "./wordlist";
+import { YoutubeSubtitle } from "./youtubeSubtitle";
+
+export default {
+    Database,
+    Image, User, Word, Wordlist, YoutubeSubtitle
+}
