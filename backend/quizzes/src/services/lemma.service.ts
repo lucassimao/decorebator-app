04ec62bf721d4855e38f906255f278f279fee25c
Lucas Simão <lsimaocosta@gmail.com>
2021-01-12 12:43:15 -0300

    it fixes getRandomLemmasForWord query

--- backend/quizzes/src/services/lemma.service.ts
+++ backend/quizzes/src/services/lemma.service.ts
@@ -13,24 +13,26 @@ const LemmaService = {
     const qBuilder = lemmaRepository
       .createQueryBuilder("lemma")
       .where((qb) => {
-        const subQuery = qb
+        const baseSubQuery = qb
           .subQuery()
-          .select(["synonym.id", "lemma.id"])
           .from(Sense, "sense")
           .innerJoin("sense.synonyms", "synonym")
           .innerJoin("sense.lemma", "lemma")
           .innerJoin("lemma.words", "word")
-          .where("word.id=:wordId", { wordId })
-          .getQuery();
+          .where("word.id=:wordId");
+
+        const subQuery1 = baseSubQuery.select("synonym.id").getSql();
+        const subQuery2 = baseSubQuery.select("lemma.id").getSql();
+        const subQuery = `${subQuery1} UNION ${subQuery2}`;
+
         return `lemma.id NOT IN (${subQuery})`;
       })
       .limit(qty)
+      .setParameters({ wordId, lexicalCategory })
       .orderBy("RANDOM()");
 
     if (lexicalCategory) {
-      qBuilder.andWhere("lemma.lexicalCategory=:lexicalCategory", {
-        lexicalCategory,
-      });
+      qBuilder.andWhere("lemma.lexicalCategory=:lexicalCategory");
     }
 
     return qBuilder.getMany();
