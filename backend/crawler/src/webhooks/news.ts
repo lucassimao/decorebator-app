8c23a12b601ec602b22b7277ffe7f818cc168c17
Lucas Simão <lsimaocosta@gmail.com>
2021-05-03 08:46:53 -0300

    fix

--- backend/crawler/src/webhooks/news.ts
+++ backend/crawler/src/webhooks/news.ts
@@ -30,7 +30,6 @@ export const newsCrawler = async (req: Request, response: Response): Promise<voi
 
         const hasEnough = await elasticSearchService.hasEnough(word.name, word.languageCode)
         if (!hasEnough){
-            response.sendStatus(200);
             const generator = new NewsCrawlerService(logger).getLatestNewsForWord(word.name);
             const articles : NewsArticle[] = []
             for await (const article of generator) {
@@ -39,8 +38,8 @@ export const newsCrawler = async (req: Request, response: Response): Promise<voi
             elasticSearchService.bulkInsert(articles,word.languageCode)
         } else {
             logger.debug(`${word.name} has enough data ... skiping`)
-            response.sendStatus(200);
         }
+        response.sendStatus(200);
     } catch (error) {
         logger.error('Error while processing word', error);
         response.sendStatus(500);
